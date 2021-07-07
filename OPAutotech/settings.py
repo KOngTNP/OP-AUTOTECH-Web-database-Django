bac806b77d482aa22406993b498ab1fcad6df643
Tanapol <tanapollll23@gmail.com>
2021-07-07 09:28:18 +0700

    set debug=true

--- OPAutotech/settings.py
+++ OPAutotech/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-(7oazmn5hwp+^fyay5prsxy++u(x!r^ctm0k5qweuhgk!a89+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://opautotech-db.herokuapp.com', 'localhost', '127.0.0.1']
 
