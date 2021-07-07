7fa305e71074a2ed55802695536f4db929c597e5
Tanapol <tanapollll23@gmail.com>
2021-07-07 16:57:20 +0700

    add Maker, Cutting and Machine funtion

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Document, Job, Drawing, Maker
+from .models import Job, Drawing, Document, Maker, Cutting, Machine
 # Register your models here.
 
 admin.site.register(Job)
 admin.site.register(Drawing)
 admin.site.register(Document)
 admin.site.register(Maker)
+admin.site.register(Cutting)
+admin.site.register(Machine)
